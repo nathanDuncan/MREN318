99c7d8516a4b28eed36686222b1d7192de929b22
dhypoon <dhypoon@outlook.com>
2023-11-22 19:19:26 -0500

    Created plotting prototype (will transfer onto Pi)

--- Project/fsr_data_process.py
+++ Project/fsr_data_process.py
@@ -1,44 +1,65 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Convert voltage reading to mass of food (g)
+
 # Arduino: read voltage and send to pi
+
 # Calibrate
-food_portion = 25               # Input from food portion from website setting
-
-# At next feeding interval time, compare leftover food mass to regular portion size
-food_leftover = 15             # 15 will be replaced from g read from arduino
-
-
-
-# Adjust next feed based on leftover food
-food_eaten = food_portion - food_leftover
-
-# Save food eaten in memory somewhere
-next_feed = food_eaten
-
-# Send next_feed to motor control
-
-
-
-
-# Track how much food has been eaten at each meal, this will be conducted on the website or somewhere with memory
 food_eaten_log = []
 food_portion_log = []
-food_eaten_log.append(food_eaten)
-i = 0
-for i in range(len(food_eaten_log)):
+
+while True:
+    food_portion = 25               # Input from food portion from website setting
+
+    # At next feeding interval time, compare leftover food mass to regular portion size
+    food_leftover = int(input("Enter food leftover: "))             # 15 will be replaced from g read from arduino
+    if(food_leftover == 66):
+        break
+
+
+
+    # Adjust next feed based on leftover food
+    food_eaten = food_portion - food_leftover
+
+    # Save food eaten in memory somewhere
+    next_feed = food_eaten
+
+    # Send next_feed to motor control
+
+
+    # Track how much food has been eaten at each meal, this will be conducted on the website or somewhere with memory
+
+    food_eaten_log.append(food_eaten)
     food_portion_log.append(food_portion)
 
-print(food_eaten)
-    
+    print("food eaten:")
+    print(food_portion)
+    print("log:")
+    print(food_eaten_log)
+    print(food_portion_log)
+
+num_feeds = [z for z in range(len(food_eaten_log))]
+
+        
+plt.plot(num_feeds,food_eaten_log)
+plt.plot(num_feeds, food_portion_log)
+plt.xlabel("Number of feeds")
+plt.ylabel("Food (g)")
+plt.ylim(0, 50)
+plt.legend(["Food eaten", "Food portion"])
+plt.show()
+
+avg_food_eaten = np.average(food_eaten_log)
+print(avg_food_eaten)
+if(food_portion - avg_food_eaten >= 5):
+    print("Your cat is not eating enough")
+elif(food_portion - avg_food_eaten < 5):
+    print("Your cat is eating well")
 
 
+    # Create a graph using track, display expected and actual
 
 
-
-
-# Create a graph using track, display expected and actual
-
-
-# Write statements about food
+    # Write statements about food
 
